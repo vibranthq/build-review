6a3981c6d6973fd3402212ebb1474d366bdc4d14
Yasuaki Uechi <y@uechi.io>
2019-09-30 18:34:12 +0900

    feat: add theme and paperSize

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,22 @@ set -e
 
 BUILD_DIR="${1}"
 DIST_DIR="${2}"
+FORMAT="${3}"
+THEME="${4}"
+PAPER_SIZE="${5}"
 
 echo "Build: ${BUILD_DIR}"
 echo "Dist: ${DIST_DIR}"
+echo "Format: ${FORMAT}"
+echo "Theme: ${THEME}"
+echo "Paper Size: ${PAPER_SIZE}"
 
 # copy articles
 cp -r "${BUILD_DIR}" /in
 
-opinionated-review pdf
+ls -al $THEME
+
+opinionated-review ${FORMAT} --theme="${THEME}" --paperSize="${PAPER_SIZE}"
 
 # push back artifacts
 mkdir -p "${DIST_DIR}"
